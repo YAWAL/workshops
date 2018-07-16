f708790db8b7f4f711a3767c30f38a2afd88c9b9
YAWAL <yavorskyyval@gmail.com>
2018-07-16 19:05:13 +0300

    faking test data

--- api/grifts/db.go
+++ api/grifts/db.go
@@ -2,10 +2,10 @@ package grifts
 
 import (
 	"github.com/markbates/grift/grift"
-	"fmt"
 	"github.com/icrowley/fake"
-	"github.com/YAWAL/workshops/api/models"
+	"time"
 	"github.com/gobuffalo/uuid"
+	"github.com/YAWAL/workshops/api/models"
 )
 
 var _ = grift.Namespace("db", func() {
@@ -14,24 +14,27 @@ var _ = grift.Namespace("db", func() {
 	grift.Add("seed", func(c *grift.Context) error {
 		// Add DB seeding stuff here
 
+		models.DB.RawQuery("Truncate table categories")
+		models.DB.RawQuery("Truncate table items")
 		for i := 0; i < 15; i++ {
 			uu, _ := uuid.NewV4()
-			fmt.Println(uu)
-			fmt.Println(fake.Title())
 			for j := 0; j < 20; j++ {
+				uu_categories, _ := uuid.NewV4()
+
 				alias:= fake.FirstName()
 				title:= fake.Title()
 				desc:= fake.Month()
 				logo:= fake.Continent()
-				models.DB.RawQuery("INSERT INTO categories (id, alias, title, descr, logo, parent_id) VALUES ($1, $2, $3, $4, $5, $6)",
-					uu,alias, title,desc,logo , uu)
-				fmt.Println(logo)
+				models.DB.RawQuery("INSERT INTO categories (id, alias, title, descr, logo, parent_id, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+					uu_categories,alias, title,desc,logo , uu, time.Now(),time.Unix(1531747237,567))
 
 			}
 
-			for j := 0; j < 150; j++ {
-				models.DB.RawQuery("INSERT INTO items (id, alias, title, descr, pictures, price, count, parent_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
-					uu, fake.FirstName(), fake.Title(), fake.Model(), fake.Brand(), fake.MonthNum(), fake.WeekdayNum(), uu)
+			for k := 0; k < 150; k++ {
+				uu_items, _ := uuid.NewV4()
+
+				models.DB.RawQuery("INSERT INTO items (id, alias, title, descr, pictures, price, count, category_id, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
+					uu_items, fake.FirstName(), fake.Title(), fake.Model(), fake.Brand(), fake.MonthNum(), fake.WeekdayNum(), uu, time.Now(),time.Unix(1531741217,567))
 			}
 
 		}
