c6c94c824f93646718cd18408c86e776d0abdda2
YAWAL <yavorskyyval@gmail.com>
2018-07-16 16:35:16 +0300

    faking test data

--- api/main.go
+++ api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/YAWAL/ERP/back-end/tool/logging"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 
+	"time"
 )
 
 // main is the starting point to your Buffalo application.
@@ -22,25 +23,27 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	conn.Exec("Truncate table categories")
+	conn.Exec("Truncate table items")
 	for i := 0; i < 15; i++ {
 		uu, _ := uuid.NewV4()
-		fmt.Println(uu)
-		fmt.Println(fake.Title())
 		for j := 0; j < 20; j++ {
+			uu_categories, _ := uuid.NewV4()
+
 			alias:= fake.FirstName()
 			title:= fake.Title()
 				desc:= fake.Month()
 				logo:= fake.Continent()
-			conn.Exec("INSERT INTO categories (id, alias, title, descr, logo, parent_id) VALUES ($1, $2, $3, $4, $5, $6)",
-				uu,alias, title,desc,logo , uu)
-			fmt.Println(logo)
+			conn.Exec("INSERT INTO categories (id, alias, title, descr, logo, parent_id, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+				uu_categories,alias, title,desc,logo , uu, time.Now(),time.Unix(1531747237,567))
 
 		}
 
-		for j := 0; j < 150; j++ {
-			conn.Exec("INSERT INTO items (id, alias, title, descr, pictures, price, count, parent_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
-				uu, fake.FirstName(), fake.Title(), fake.Model(), fake.Brand(), fake.MonthNum(), fake.WeekdayNum(), uu)
+		for k := 0; k < 150; k++ {
+			uu_items, _ := uuid.NewV4()
+
+			conn.Exec("INSERT INTO items (id, alias, title, descr, pictures, price, count, category_id, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)",
+				uu_items, fake.FirstName(), fake.Title(), fake.Model(), fake.Brand(), fake.MonthNum(), fake.WeekdayNum(), uu, time.Now(),time.Unix(1531741217,567))
 		}
 
 	}
